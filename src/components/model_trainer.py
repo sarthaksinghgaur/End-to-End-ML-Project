d625fc8cf2367148df567d6a8cafa34237097175
Sarthak Singh Gaur <s7s5g4@gmail.com>
2025-05-18 16:46:24 +0530

    model trainer

---
+++ src/components/model_trainer.py
@@ -0,0 +1,70 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from sklearn.ensemble import (
+    RandomForestClassifier,
+    GradientBoostingClassifier,
+    AdaBoostClassifier
+)
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from xgboost import XGBClassifier
+from catboost import CatBoostClassifier
+
+from sklearn.metrics import f1_score
+
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object,evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifact", "model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self,train_array,test_array):
+        try:
+            logging.info("Split training and test input data")
+            X_train,y_train,X_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            models = {
+                "Random Forest": RandomForestClassifier()
+            }
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models)
+            
+            ## To get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
+
+            ## To get best model name from dict
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+            
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=models
+            )
+
+            predicted=best_model.predict(X_test)
+
+            f1score = f1_score(y_test, predicted)
+            return f1score
+            
+        except Exception as e:
+            raise CustomException(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -4,12 +4,15 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
-import pickle
 
-from sklearn.metrics import r2_score
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import f1_score
+
+from sklearn.model_selection import StratifiedKFold, RandomizedSearchCV
 
 from src.exception import CustomException
+
+import warnings
+warnings.filterwarnings("ignore")
 
 def save_object(file_path, obj):
     try:
@@ -22,7 +25,25 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models):
+    try:
+        report = {}
 
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = f1_score(y_train, y_train_pred, average='binary')
+            test_model_score = f1_score(y_test, y_test_pred, average='binary')
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
